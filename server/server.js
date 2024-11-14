777e5ed1c0f62096386f9b00e7e128a14849a1bf
Shrijan R <24738299+shrijanr@users.noreply.github.com>
2024-11-13 20:08:10 -0500

    Reorganized file structure, added home button to login/register screens.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../client/public')));
+app.use('/src', express.static(path.join(__dirname, '../client/src')));
 
 // Middleware to parse JSON data
 app.use(express.json());
