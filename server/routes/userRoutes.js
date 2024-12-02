0a31a5872d866caeb90cca5e9f89bc3ffddc5a79
briezzy <133739556+briezzy@users.noreply.github.com>
2024-12-02 17:26:46 -0500

    Update userRoutes.js
    Changed and added a couple of things to help with authentification

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const auth = require('../middleware/authentication');
 
+// Public routes
 // Register a new user
 router.post('/register', UserController.register);
 
@@ -10,5 +12,11 @@ router.post('/login', UserController.login);
 
 // Follow another user
 router.post('/:userId/follow', UserController.follow);
+
+
+// Protected routes
+router.get('/profile', auth, (req, res) => {
+  res.status(200).json({ message: 'Welcome to your profile!', user: req.user });
+});
 
 module.exports = router;
